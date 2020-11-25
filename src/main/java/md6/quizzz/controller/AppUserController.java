c1d6dfddac9e6e7e730e37e1be8e2e61a20e81fd
JavaHater <vanthanhtung1562013@gmail.com>
2020-11-25 16:49:55 +0700

    add Quiz controller

--- src/main/java/md6/quizzz/controller/AppUserController.java
+++ src/main/java/md6/quizzz/controller/AppUserController.java
@@ -1,7 +1,63 @@
 package md6.quizzz.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import md6.quizzz.model.AppUser;
+import md6.quizzz.model.Exam;
+import md6.quizzz.service.appUserService.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/users")
 public class AppUserController {
+
+    @Autowired
+    private AppUserService appUserService;
+
+    @GetMapping()
+    public ResponseEntity<Iterable<AppUser>> findAll(){
+        return new ResponseEntity<>(appUserService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<AppUser>> findById(@PathVariable Long id){
+        Optional<AppUser> appUser = appUserService.findById(id);
+        if (!appUser.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(appUser, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<AppUser> save(@RequestBody AppUser appUser) {
+        appUserService.save(appUser);
+        return new ResponseEntity<>(appUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<AppUser> edit(@PathVariable Long id, @RequestBody AppUser appUser){
+        Optional<AppUser> currentAppUser = appUserService.findById(id);
+        if (!currentAppUser.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        currentAppUser.get().setAddress(appUser.getAddress());
+        currentAppUser.get().setAge(appUser.getAge());
+        currentAppUser.get().setEmail(appUser.getEmail());
+        currentAppUser.get().setImage(appUser.getImage());
+        currentAppUser.get().setPassword(appUser.getPassword());
+        currentAppUser.get().setRole(appUser.getRole());
+        currentAppUser.get().setUsername(appUser.getUsername());
+        appUserService.save(currentAppUser.get());
+        return new ResponseEntity<>(currentAppUser.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<AppUser> delete(@PathVariable Long id){
+        appUserService.remove(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/md6/quizzz/controller/QuizController.java
+++ src/main/java/md6/quizzz/controller/QuizController.java
@@ -1,5 +1,6 @@
 package md6.quizzz.controller;
 
+import md6.quizzz.model.Exam;
 import md6.quizzz.model.Quiz;
 import md6.quizzz.repository.QuizRepository;
 import md6.quizzz.service.categoryService.CategoryService;
@@ -22,12 +23,12 @@ public class QuizController {
     CategoryService categoryService;
 
     @GetMapping()
-    public ResponseEntity<Iterable<Quiz>> getAll(){
+    public ResponseEntity<Iterable<Quiz>> findAll(){
         return new ResponseEntity<>(quizService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Quiz>> getById(@PathVariable Long id){
+    public ResponseEntity<Optional<Quiz>> findById(@PathVariable Long id){
         Optional<Quiz> quiz = quizService.getById(id);
         if (!quiz.isPresent()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -36,8 +37,32 @@ public class QuizController {
     }
 
     @PostMapping()
-    public ResponseEntity<Quiz> add(@RequestBody Quiz quiz){
+    public ResponseEntity<Quiz> save(@RequestBody Quiz quiz){
         quizService.save(quiz);
         return new ResponseEntity<>(quiz, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Quiz> edit(@PathVariable Long id, @RequestBody Quiz quiz){
+        Optional<Quiz> currentQuiz = quizService.getById(id);
+        if (!currentQuiz.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        currentQuiz.get().set_active(quiz.is_active());
+        currentQuiz.get().setCategory(quiz.getCategory());
+        currentQuiz.get().setContent(quiz.getContent());
+        currentQuiz.get().setCreate_at(quiz.getCreate_at());
+        currentQuiz.get().setExam(quiz.getExam());
+        currentQuiz.get().setLevel(quiz.getLevel());
+        currentQuiz.get().setScore(quiz.getScore());
+        currentQuiz.get().setType(quiz.getType());
+        quizService.save(currentQuiz.get());
+        return new ResponseEntity<>(currentQuiz.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Exam> delete(@PathVariable Long id){
+        quizService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
