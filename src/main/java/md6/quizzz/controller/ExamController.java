73f7db3da0f530823680bab93ce0d141ffaddb89
Nghia <vunghia2000@gmail.com>
2020-11-26 08:57:40 +0700

    validate

--- src/main/java/md6/quizzz/controller/ExamController.java
+++ src/main/java/md6/quizzz/controller/ExamController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 
@@ -18,14 +17,14 @@ public class ExamController {
     ExamService examService;
 
     @GetMapping()
-    public ResponseEntity<Iterable<Exam>> findAll(){
+    public ResponseEntity<Iterable<Exam>> findAll() {
         return new ResponseEntity<>(examService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Exam>> findById(@PathVariable Long id){
+    public ResponseEntity<Optional<Exam>> findById(@PathVariable Long id) {
         Optional<Exam> exam = examService.findById(id);
-        if (!exam.isPresent()){
+        if (!exam.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(exam, HttpStatus.OK);
@@ -33,26 +32,32 @@ public class ExamController {
 
     @PostMapping()
     public ResponseEntity<Exam> save(@RequestBody Exam exam) {
-        examService.save(exam);
-        return new ResponseEntity<>(exam, HttpStatus.CREATED);
+        if (examService.validate(exam)) {
+            examService.save(exam);
+            return new ResponseEntity<>(exam, HttpStatus.CREATED);
+        }
+        else return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Exam> edit(@PathVariable Long id, @RequestBody Exam exam){
+    public ResponseEntity<Exam> edit(@PathVariable Long id, @RequestBody Exam exam) {
         Optional<Exam> currentExam = examService.findById(id);
-        if (!currentExam.isPresent()){
+        if (!currentExam.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else if (examService.validate(exam)){
+            currentExam.get().setCreated_at(exam.getCreated_at());
+            currentExam.get().setDuration(exam.getDuration());
+            currentExam.get().setScore(exam.getScore());
+            currentExam.get().setCategory(exam.getCategory());
+            examService.save(currentExam.get());
+            return new ResponseEntity<>(currentExam.get(), HttpStatus.OK);
         }
-        currentExam.get().setCreated_at(exam.getCreated_at());
-        currentExam.get().setDuration(exam.getDuration());
-        currentExam.get().setScore(exam.getScore());
-        currentExam.get().setCategory(exam.getCategory());
-        examService.save(currentExam.get());
-        return new ResponseEntity<>(currentExam.get(), HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Exam> delete(@PathVariable Long id){
+    public ResponseEntity<Exam> delete(@PathVariable Long id) {
         examService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- src/main/java/md6/quizzz/service/examService/ExamServiceImpl.java
+++ src/main/java/md6/quizzz/service/examService/ExamServiceImpl.java
@@ -4,6 +4,8 @@ import md6.quizzz.model.Exam;
 import md6.quizzz.repository.ExamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
 import java.util.Optional;
 
 @Service
@@ -29,5 +31,14 @@ public class ExamServiceImpl implements ExamService {
     @Override
     public void deleteById(Long id) {
         examRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean validate(Exam exam) {
+        if (exam.getCategory() == null) return false;
+        if (exam.getDuration() <= 0) return false;
+        if (exam.getExam_code() == null || exam.getExam_code().equals("")) return false;
+        if (exam.getExam_name() == null || exam.getExam_name().equals("")) return false;
+        return exam.getScore() > 0;
     }
 }
