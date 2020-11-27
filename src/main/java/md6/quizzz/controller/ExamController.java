22a53fff77a6eabe2c819e5171e028164aa95ef5
nghiapham137 <phamanhnghia13794@gmail.com>
2020-11-27 15:58:51 +0700

    add preAuthorize

--- src/main/java/md6/quizzz/controller/ExamController.java
+++ src/main/java/md6/quizzz/controller/ExamController.java
@@ -5,6 +5,7 @@ import md6.quizzz.service.examService.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
 @RestController
 @CrossOrigin
 @RequestMapping("/exams")
+@PreAuthorize("hasRole('ADMIN')")
 public class ExamController {
     @Autowired
     ExamService examService;
